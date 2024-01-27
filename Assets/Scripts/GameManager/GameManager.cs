672770639861c60dba91f1bdaed11ddbde565a98
Maarten Braaksma <webmaster@maartenbraaksma.nl>
2024-01-27 17:36:47 +0100

    Iterate player
    - Execute haptic feedbacks on farting.

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,3 +1,4 @@
+using CodeKriebels.Player;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Scripts/Player/PlayerFart.cs
+++ Assets/Scripts/Player/PlayerFart.cs
@@ -3,16 +3,27 @@ namespace CodeKriebels.Player
     using CodeKriebels.Audio;
     using System.Collections;
     using UnityEngine;
-    using UnityEngine.VFX;
 
     public class PlayerFart : MonoBehaviour
     {
         [Header("References")]
-        [SerializeField, Tooltip("A reference to the visual effect.")]
-        private VisualEffect visualEffect;
+        [SerializeField, Tooltip("A reference to the particle system.")]
+        private ParticleSystem particleSystem;
 
         [SerializeField, Tooltip("The interval for farts")]
         private Vector2 fartInterval = new Vector2(0.5f, 1f);
+
+        [Header("Haptic Settings")]
+        [SerializeField, Tooltip("The left motor haptic feedback speed.")]
+        private float hapticLowFrequency = 0.25f;
+
+        [SerializeField, Tooltip("The right motor haptic feedback speed.")]
+        private float hapticHighFrequency = 0.75f;
+
+        [SerializeField, Tooltip("The haptic duration.")]
+        private float hapticDuration = 1f;
+
+        internal Player parent;
 
 
         private void Awake()
@@ -26,15 +37,15 @@ namespace CodeKriebels.Player
             {
                 yield return new WaitForSeconds(Random.Range(fartInterval.x, fartInterval.y));
 
-                visualEffect.Play();
-                FartHandler.Instance.PlayFart(FartHandler.FartSize.Small);
+                ExecuteFart(FartHandler.FartSize.Small);
             }
         }
 
         internal void ExecuteFart(FartHandler.FartSize size)
         {
-            visualEffect.Play();
+            particleSystem.Play();
             FartHandler.Instance.PlayFart(size);
+            parent.ExecuteHapticFeedback(hapticLowFrequency, hapticHighFrequency, hapticDuration);
         }
     }
 }
