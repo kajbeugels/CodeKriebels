cd5073c7f09fa255e28e549a3c5540d25be85c7f
Geoffrey Hendrikx <geoffreyhendrikx@hotmail.com>
2024-01-27 13:18:27 +0100

    Telephone shit

---
+++ Assets/Scripts/Enviroment/Telephone.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.Timers;
+
+public class Telephone : MonoBehaviour
+{
+    [SerializeField]
+    private bool isRinging = false;
+
+
+    [SerializeField]
+    private int maxTime;
+    [SerializeField]
+    private int minTime;
+
+    private void Start()
+    {
+        TimerManager.Instance.AddTimer(ToggleRinging, Random.Range(minTime, maxTime));
+    }
+
+    private void ToggleRinging()
+    {
+        isRinging = !isRinging;
+        TimerManager.Instance.AddTimer(ToggleRinging, Random.Range(minTime, maxTime));
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isRinging && other.TryGetComponent(out Player player))
+        {
+            
+        }
+    }
+}
