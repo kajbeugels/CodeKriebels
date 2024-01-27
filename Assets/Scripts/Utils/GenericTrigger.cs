7c2d928476a367b9ae713874a364e630c4b9fe14
Geoffrey Hendrikx <geoffreyhendrikx@hotmail.com>
2024-01-27 13:18:27 +0100

    Telephone shit

--- Assets/Scripts/Utils/GenericTrigger.cs
+++ Assets/Scripts/Utils/GenericTrigger.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Collider))]
 public class GenericTrigger : MonoBehaviour
 {
     [SerializeField]
-    private UnityEvent OnTrigger_Enter;
+    private UnityEvent OnTrigger_Enter; 
+    [SerializeField]
+    private UnityEvent OnTrigger_Stay; 
+    [SerializeField]
+    private UnityEvent OnTrigger_Exit;
+
     private void OnTriggerEnter(Collider other)
     {
         OnTrigger_Enter.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        OnTrigger_Stay.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnTrigger_Exit.Invoke();
     }
 }
