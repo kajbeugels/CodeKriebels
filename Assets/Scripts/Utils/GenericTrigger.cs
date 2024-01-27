cb6fe31e3914910e59fd7e770578d12565e90766
Geoffrey Hendrikx <geoffreyhendrikx@hotmail.com>
2024-01-27 12:13:04 +0100

    water reserviour trigger

--- Assets/Scripts/Utils/GenericTrigger.cs
+++ Assets/Scripts/Utils/GenericTrigger.cs
@@ -7,25 +7,8 @@ public class GenericTrigger : MonoBehaviour
 {
     [SerializeField]
     private UnityEvent OnTrigger_Enter;
-    [SerializeField]
-    private UnityEvent OnTrigger_Stay;
-    [SerializeField]
-    private UnityEvent OnTrigger_Exit;
-
     private void OnTriggerEnter(Collider other)
     {
         OnTrigger_Enter.Invoke();
     }
-
-    private void OnTriggerStay(Collider other)
-    {
-        OnTrigger_Stay.Invoke();
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        OnTrigger_Exit.Invoke();
-    }
-
-
 }
