063cc54548563be6f8e62763827c8e59bbe0c486
Geoffrey Hendrikx <geoffreyhendrikx@hotmail.com>
2024-01-27 09:30:53 +0100

    Generic trigger

---
+++ Assets/Scripts/Utils/GenericTrigger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GenericTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent OnTrigger_Enter;
+    [SerializeField]
+    private UnityEvent OnTrigger_Stay;
+    [SerializeField]
+    private UnityEvent OnTrigger_Exit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTrigger_Enter.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        OnTrigger_Stay.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnTrigger_Exit.Invoke();
+    }
+
+
+}
