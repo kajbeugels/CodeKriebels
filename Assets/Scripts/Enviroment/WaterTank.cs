9621bb2cb8ce96fa7fb0ccb482e5f1a3fade05b0
Geoffrey Hendrikx <geoffreyhendrikx@hotmail.com>
2024-01-28 11:11:39 +0100

    New WaterTank prefab

---
+++ Assets/Scripts/Enviroment/WaterTank.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaterTank : MonoBehaviour
+{
+    [SerializeField]
+    private float upFactor;
+    [SerializeField]
+    private GameObject Water;
+    [SerializeField]
+    private Transform spawnPosition;
+    private bool isSpawned = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.up.y < upFactor && !isSpawned)
+        {
+            Water.SetActive(true);
+            Water.transform.position = new Vector3(spawnPosition.position.x,0,spawnPosition.position.z);
+            isSpawned = true;
+        }
+    }
+}
